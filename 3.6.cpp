c1f97f0dd342110ae553184140bed63689a04ce0
franklin183 <135258695+franklin183@users.noreply.github.com>
2023-06-14 13:15:58 -0500

    Update 3.6.cpp
    corrección de yardas a pulgas

--- 3.6.cpp
+++ 3.6.cpp
@@ -1,18 +1,13 @@
 #include <iostream>
 using namespace std;
 int main(){
-    float pies, pulgadas, yardas, metros, centimetros;
-    cout << " Introduzca pies: ";
-    cin >> pies;
-    pulgadas = pies * 12;
-    yardas = pies / 3;
-    centimetros = pulgadas * 2.54;
-    metros = centimetros / 100;
-    cout << " pies " << pies << endl;
-    cout << " pulgadas " << pulgadas << endl;
-    cout << " yardas " << yardas << endl;
-    cout << " centimetros " << centimetros << endl;
-    cout << " metros " << metros << endl;
+float yardas, pulgadas ;
+cout << " Introduzca yardas: ";
+cin >> yardas ;
+pulgadas = yardas * 12;
+yardas = pulgadas / 3;
+cout << " pulgadas " << pulgadas << endl;
+cout << " yardas " << yardas << endl;
 
 return 0;
 } // REALIZAR LA TRANSFORMACION DE YARDAS A PULGADAS
